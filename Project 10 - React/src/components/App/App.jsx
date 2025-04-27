e47b8d09615d702e8f0e26caee4f5b6b38afaebe
Franklin Velasquez <velasqfr96@gmail.com>
2025-04-27 19:38:00 -0400

    Css changes in header, page, card, weather

--- Project 10 - React/src/components/App/App.jsx
+++ Project 10 - React/src/components/App/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
-  return <div className="app">Hello</div>;
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+        <Main weatherData={weatherData} />
+      </div>
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;

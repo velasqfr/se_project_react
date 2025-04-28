58a78bebb45c1aeb9ca622211a0a68b897965ff3
Franklin Velasquez <velasqfr96@gmail.com>
2025-04-27 21:06:28 -0400

    created submission form

---
+++ Project 10 - React/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p className="footer__text">Developed by Franklin Velasquez</p>
+      <p className="footer__text_year">© 2025</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Project 10 - React/src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,24 @@
+import "./ModalWithForm.css";
+
+function ModalWithForm({ children, buttonText, title }) {
+  return (
+    <div className="modal">
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button type="button" className="modal__close">
+          CLOSE
+        </button>
+        <form className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
+
+/*htmlFor should match the ID number */

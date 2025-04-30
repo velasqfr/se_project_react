dcb26e092acf3e7530a24a579aebbc4d9112cad3
Franklin Velasquez <velasqfr96@gmail.com>
2025-04-30 11:14:28 -0400

    Updated modal_opened

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -3,7 +3,7 @@ import close from "../../assets/close.png";
 
 function ItemModal({ activeModal, onClose, card }) {
   return (
-    <div className={`modal ${activeModal === "preview" && "modal__opened"}`}>
+    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
           <img src={close} alt="close modal" />
